2ce5417a0ed680f80cec12e4158588e8007da7e0
tsuzukihashi <tsudukihashi0817@gmail.com>
2020-10-08 02:49:53 +0900

    プロジェクト作成

---
+++ MarubatsuGame/MarubatsuGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarubatsuGameApp.swift
+//  MarubatsuGame
+//
+//  Created by tsuzuki817 on 2020/10/07.
+//
+
+import SwiftUI
+
+@main
+struct MarubatsuGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
