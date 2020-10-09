ce394a750515dc09e7a79c2fcf204d7aa6b9cf59
tsuzukihashi <tsudukihashi0817@gmail.com>
2020-10-10 02:43:35 +0900

    タップして色を変える

--- MarubatsuGame/HomeView.swift
+++ MarubatsuGame/HomeView.swift
@@ -1,16 +1,47 @@
 import SwiftUI
 
 struct HomeView: View {
+    @State var moves: [String] = Array(repeating: "", count: 9)
+    @State var isPlaying = false
     var body: some View {
         VStack {
-            LazyVGrid(
-                columns: Array(repeating: GridItem(.flexible() ,spacing: 8), count: 3),
-                spacing: 8
-            ) {
-                ForEach(0..<9, id: \.self) { index in
-                    Color.white
-                }
-            }.padding()
+            GeometryReader { geometry in
+                LazyVGrid(
+                    columns: Array(repeating: GridItem(.flexible() ,spacing: 4), count: 3),
+                    spacing: 16
+                ) {
+                    ForEach(0..<9, id: \.self) { index in
+                        ZStack {
+                            Color.blue
+                            
+                            Color.white
+                                .opacity(moves[index] == "" ? 1 : 0)
+                            Text(moves[index])
+                                .font(.largeTitle)
+                                .fontWeight(.heavy)
+                                .foregroundColor(.white)
+                                .opacity(moves[index] != "" ? 1 : 0)
+                        }
+                        .frame(width: geometry.size.width / 4, height: geometry.size.width / 4)
+                        .rotation3DEffect(
+                            .init(degrees: moves[index] != "" ? 180 : 0),
+                            axis: (x: 0.0, y: 1.0, z: 0.0),
+                            anchor: .center,
+                            anchorZ: 0.0,
+                            perspective: 1.0
+                        )
+                        .cornerRadius(16)
+                        .onTapGesture(perform: {
+                            withAnimation(Animation.easeIn(duration: 0.5)) {
+                                if moves[index] == "" {
+                                    moves[index] = isPlaying ? "X" : "O"
+                                    isPlaying.toggle()
+                                }
+                            }
+                        })
+                    }
+                }.padding()
+            }
         }
     }
 }
